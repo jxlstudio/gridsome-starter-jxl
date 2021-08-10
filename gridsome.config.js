2164623956342964c8a5ac7addc9aa32a46f8cb9
jxlstudio <jeremy@jxl.io>
2021-08-10 16:23:12 -0400

    svg icons and updated gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,6 +8,22 @@ module.exports = {
   siteName: 'Gridsome',
   plugins: [
     {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: process.env.GRIDSOME_GOOGLE_ANALYTICS_ID
+      }
+    },
+    {
+      use: '@gridsome/source-contentful',
+      options: {
+        space: process.env.GRIDSOME_CTFL_SPACE_ID, // required
+        accessToken: process.env.GRIDSOME_API_KEY, // required
+        host: 'cdn.contentful.com',
+        environment: 'master',
+        typeName: 'Contentful'
+      }
+    },
+    {
       use: "gridsome-plugin-tailwindcss",
       /**
       * These are the default options.
@@ -20,5 +36,28 @@ module.exports = {
       }
       */
     },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        // exclude: [],
+        cacheTime: 60000,
+        config: {
+          '/*': {
+            changefreq: 'monthly',
+            priority: 0.5
+          }
+        }
+      }
+    }
   ],
+  chainWebpack (config) {
+    config.mode(process.env.GRIDSOME_WEBPACK_CONFIG_MODE)
+
+    // Load SVG Icons as components
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+	}
 }
